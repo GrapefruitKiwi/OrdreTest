8e01b28b6eb891a3120a7277f8220d17005bd19b
GrapefruitKiwi <kaiweidong2001@gmail.com>
2025-06-30 17:59:31 +0100

    up

---
+++ dist_sushi/server.js
@@ -0,0 +1,139 @@
+const express = require("express");
+const compression = require('compression');
+const cors = require('cors');
+const http = require("http");
+const https = require('https');
+const fs = require('fs');
+const { Server } = require("socket.io");
+const path = require("path");
+const menuController = require('./controllers/menuController.js');
+const uploadController = require('./controllers/uploadController.js');
+const socketService = require('./socket/socketService.js');
+const uploadMiddleware = require('./middlewares/uploadMiddleware.js');
+const appStateService = require('./services/appStateService.js')
+const { logger } = require('./utils/logger.js')
+const {appState} = require("./state");
+const holiday = require('./utils/holiday.js')
+const { initUserData, saveUserData } = require('./services/userService.js')
+const app = express();
+app.use(cors());
+app.use(compression());
+
+// 路由只保留上传接口
+app.post('/upload', uploadMiddleware.any(), uploadController.handleUpload);
+
+// 创建 HTTP 服务器和 Socket.IO
+let server;
+const usedHttps = process.env.USE_HTTPS || "false";
+
+if (usedHttps == "true")
+{
+  let key_name = "order";
+  if (process.env.HTTPS_HEAD) {
+    key_name = process.env.HTTPS_HEAD;
+  }
+
+  let file_key = '/etc/letsencrypt/live/' + key_name + '.xiaoxiong.pt/privkey.pem';
+  let file_cert = '/etc/letsencrypt/live/' + key_name + '.xiaoxiong.pt/fullchain.pem';
+
+  logger.info(file_key);
+  logger.info(file_cert);
+
+  // 配置 HTTPS 选项
+  const httpsOptions = {
+    key: fs.readFileSync(file_key),
+    cert: fs.readFileSync(file_cert),
+  };
+  server = https.createServer(httpsOptions, app);
+
+  app.use((req, res, next) => {
+    if (!req.secure) {
+      // 自动重定向 HTTP 到 HTTPS
+      return res.redirect(`https://${req.headers.host}${req.url}`);
+    }
+    next();
+  });
+
+  // 创建 HTTP 服务器（用于重定向）
+  // app.listen(80, () => {
+  //   logger.info(`HTTP server running on port 80`)
+  // });
+}
+else
+{
+  server = http.createServer(app);
+}
+
+
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ["GET", "POST"]
+  }
+});
+
+app.use(compression());
+app.use(express.static(path.join(__dirname, "public"), {
+  setHeaders: (res, filePath) => {
+    if (filePath.endsWith('.gz')) {
+      res.set('Content-Encoding', 'gzip');
+    }
+  }
+}));
+
+async function main() {
+  await initUserData();
+  appStateService.loadAppState();
+  menuController.loadMenu();
+  socketService.init(io);
+
+  const PORT = process.env.PORT || 80;
+  server.listen(PORT, () => {
+    logger.info(`🟢 服务器已启动，监听端口 ${PORT}`);
+  });
+
+holiday.updateToday(appState);
+runInterval();
+}
+
+main();
+// 捕获关闭信号时保存数据
+process.on("SIGINT", () => {
+  logger.info(`🛑 收到退出信号，正在保存数据...`)
+  appStateService.saveAppState();
+  saveUserData()
+  process.exit(0);
+});
+
+process.on("SIGTERM", () => {
+  logger.info("\n🛑 收到终止信号，正在保存数据...");
+  appStateService.saveAppState();
+  saveUserData()
+  process.exit(0);
+});
+
+let needClean = true;
+
+function runInterval() {
+  setTimeout(() => {
+    const now = new Date();
+    if (now.getHours() == 1)
+    {
+      if ( needClean ) {
+        logger.info('自动清除订单和更新红日')
+        appState.clearAll();
+
+        // update today for appState.isFestiveDay
+        holiday.updateToday(appState);
+      }
+      needClean = false;
+    }
+    else
+    {
+      needClean = true;
+    }
+
+    runInterval();
+  }, 1000 * 60);
+}
+
---
+++ sushi_configfiles/saldanha.config.js
@@ -0,0 +1,22 @@
+// ecosystem.config.js
+module.exports = {
+  apps: [{
+    name: "SS_SALDANHA",
+    script: "./server.js",
+    cwd: "../dist_sushi",
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: "800M",
+    env: {
+      NODE_ENV: "production",
+      USE_HTTPS: false,
+      HTTPS_HEAD: "order",
+      PORT: 500,
+      QR_ADDR: "https://109.199.107.30:500/client.html?table=",
+      TABLE_NUMBER: [[1, 35]],
+      SAVE_ADDR: "save/ss_saldanha",
+      ENABLE_ROAST_DUCK: false,
+    }
+  }]
+}
